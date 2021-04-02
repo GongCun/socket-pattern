b2a8d381a976a5e93aaffeffdfe6b0b7420e04b1
Gong Cun <gongcunjust@gmail.com>
2021-04-02 15:59:52 +0800

    update

--- tcpserver2.cpp
+++ tcpserver2.cpp
@@ -10,6 +10,8 @@
 #include <sys/select.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 using namespace std;
 using SA = struct sockaddr;
@@ -17,15 +19,46 @@ const int SERV_PORT = 9989;
 const int LISTENQ = 5;
 const int MAXLINE = 4096;
 
+void err_exit(const char *s) {
+    perror(s);
+    exit(-1);
+}
+
+void sig_chld(int signo) {
+    while (waitpid(-1, (int *)NULL, WNOHANG) > 0)
+        ;
+    return;
+}
+
+void doit(int sockfd) {
+    int len;
+    char buf[MAXLINE];
+
+  again:
+    while ((len = read(sockfd, buf, MAXLINE)) > 0) {
+        int ret;
+        char *ptr = buf;
+        while (len != 0 && (ret = write(sockfd, ptr, len)) != 0) {
+            if (ret == -1) {
+                if (errno == EINTR)
+                    continue;
+                err_exit("write");
+            }
+            len -= ret;
+            ptr += ret;
+        }
+    }
+
+    if (len < 0 && errno == EINTR)
+        goto again;
+    else if (len < 0) {
+        err_exit("read");
+    }
+}
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in servaddr;
 
-    auto err_exit = [](const char *s) {
-        perror(s);
-        exit(-1);
-    };
-    
     int listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd < 0)
         err_exit("socket");
@@ -47,90 +80,27 @@ int main(int argc, char *argv[]) {
     if (listen(listenfd, LISTENQ) < 0)
         err_exit("listen");
 
-    char buf[MAXLINE];
-    int maxfd = listenfd;               // initialize
-    int maxi = -1;                      // index into client[]
-    vector<int> client(FD_SETSIZE, -1); // -1 indicates available entry
-    fd_set allset;
-
-    FD_ZERO(&allset);
-    FD_SET(listenfd, &allset);
-
-    auto close_sock = [&](int sockfd, int i) {
-        if (close(sockfd) < 0)
-            err_exit("close");
-        FD_CLR(sockfd, &allset);
-        client[i] = -1;
-    };
+    if (signal(SIGCHLD, sig_chld) == SIG_ERR)
+        err_exit("signal");
 
     for ( ; ; ) {
-        fd_set rset = allset;
-        int nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
-        if (nready < 0)
-            err_exit("select");
-
-        if (FD_ISSET(listenfd, &rset)) {
-            int connfd = accept(listenfd, (SA *)NULL, NULL);
-            if (connfd < 0)
-                err_exit("accept");
-
-            int i = 0;
-            for (; i < client.size(); ++i) {
-                if (client[i] < 0) {
-                    client[i] = connfd;
-                    break;
-                }
-            }
-            if (i >= client.size()) {
-                cerr << "too many client" << endl;
-                exit(-1);
-            }
-
-            FD_SET(connfd, &allset); // add new descriptor to set
-            maxfd = max(maxfd, connfd);
-            maxi = max(maxi, i);     // max index in client[]
-
-            if (--nready <= 0)
-                continue;            // no more readable descriptors
+        int connfd = accept(listenfd, (SA *)NULL, NULL);
+        if (connfd < 0) {
+            if (errno == EINTR)
+                continue; // back to fork()
+            err_exit("accept");
         }
 
-        int sockfd;
-        for (int i = 0; i <= maxi; ++i) {
-            if ((sockfd = client[i]) < 0)
-                continue;
-
-            if (FD_ISSET(sockfd, &rset)) {
-                int len = read(sockfd, buf, MAXLINE);
-                if (len <= 0) {
-                    if (len < 0)
-                        perror("read");
-                    close_sock(sockfd, i);
-                } else {
-                    int ret;
-                    char *ptr = buf;
-                    //
-                    // For sockets, a loop may be required to guarantee that you
-                    // really write out all of the requested bytes.
-                    //
-                    while (len != 0 && (ret = write(sockfd, ptr, len)) != 0) {
-                        if (ret == -1) {
-                            if (errno == EINTR)
-                                continue;
-
-                            perror("write");
-                            close_sock(sockfd, i);
-                            break;
-                        }
-
-                        len -= ret;
-                        ptr += ret;
-                    }
-                }
-
-                if (--nready <= 0)
-                    break; // no more readable descriptors
-            }
-        } // end of check
-    } // loop end
-
+        pid_t pid = fork();
+        if (pid < 0)
+            err_exit("fork");
+        else if (pid == 0) {       // child process
+            if (close(listenfd) < 0)
+                err_exit("close"); // close listening socket
+            doit(connfd);          // process request
+            exit(0);               // child process exit
+        }
+        if (close(connfd) < 0)     // parent closes connected socket
+            err_exit("close");
+    }
 }
